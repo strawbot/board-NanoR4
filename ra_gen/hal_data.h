8330a1107d4ba24c304240141eb03808b26d207d
Robert Chapman <rc@interbaun.com>
2026-04-17 03:58:05 -0600

    Nano R4 project; PlatformIO + RASC

---
+++ ra_gen/hal_data.h
@@ -0,0 +1,44 @@
+/* generated HAL header file - do not edit */
+#ifndef HAL_DATA_H_
+#define HAL_DATA_H_
+#include <stdint.h>
+#include "bsp_api.h"
+#include "common_data.h"
+#include "r_dmac.h"
+#include "r_transfer_api.h"
+#include "r_dtc.h"
+#include "r_transfer_api.h"
+#include "r_sci_uart.h"
+            #include "r_uart_api.h"
+FSP_HEADER
+/* Transfer on DMAC Instance. */
+extern const transfer_instance_t g_transfer0;
+
+/** Access the DMAC instance using these structures when calling API functions directly (::p_api is not used). */
+extern dmac_instance_ctrl_t g_transfer0_ctrl;
+extern const transfer_cfg_t g_transfer0_cfg;
+
+#ifndef NULL
+void NULL(transfer_callback_args_t * p_args);
+#endif
+/* Transfer on DTC Instance. */
+extern const transfer_instance_t g_transfer1;
+
+/** Access the DTC instance using these structures when calling API functions directly (::p_api is not used). */
+extern dtc_instance_ctrl_t g_transfer1_ctrl;
+extern const transfer_cfg_t g_transfer1_cfg;
+/** UART on SCI Instance. */
+            extern const uart_instance_t      g_uart2;
+
+            /** Access the UART instance using these structures when calling API functions directly (::p_api is not used). */
+            extern sci_uart_instance_ctrl_t     g_uart2_ctrl;
+            extern const uart_cfg_t g_uart2_cfg;
+            extern const sci_uart_extended_cfg_t g_uart2_cfg_extend;
+
+            #ifndef cli_rxtx
+            void cli_rxtx(uart_callback_args_t * p_args);
+            #endif
+void hal_entry(void);
+void g_hal_init(void);
+FSP_FOOTER
+#endif /* HAL_DATA_H_ */
---
+++ ra_gen/vector_data.h
@@ -0,0 +1,32 @@
+/* generated vector header file - do not edit */
+        #ifndef VECTOR_DATA_H
+        #define VECTOR_DATA_H
+        #ifdef __cplusplus
+        extern "C" {
+        #endif
+                /* Number of interrupts allocated */
+        #ifndef VECTOR_DATA_IRQ_COUNT
+        #define VECTOR_DATA_IRQ_COUNT    (4)
+        #endif
+        /* ISR prototypes */
+        void sci_uart_rxi_isr(void);
+        void sci_uart_txi_isr(void);
+        void sci_uart_tei_isr(void);
+        void sci_uart_eri_isr(void);
+
+        /* Vector table allocations */
+        #define VECTOR_NUMBER_SCI2_RXI ((IRQn_Type) 0) /* SCI2 RXI (Receive data full) */
+        #define SCI2_RXI_IRQn          ((IRQn_Type) 0) /* SCI2 RXI (Receive data full) */
+        #define VECTOR_NUMBER_SCI2_TXI ((IRQn_Type) 1) /* SCI2 TXI (Transmit data empty) */
+        #define SCI2_TXI_IRQn          ((IRQn_Type) 1) /* SCI2 TXI (Transmit data empty) */
+        #define VECTOR_NUMBER_SCI2_TEI ((IRQn_Type) 2) /* SCI2 TEI (Transmit end) */
+        #define SCI2_TEI_IRQn          ((IRQn_Type) 2) /* SCI2 TEI (Transmit end) */
+        #define VECTOR_NUMBER_SCI2_ERI ((IRQn_Type) 3) /* SCI2 ERI (Receive error) */
+        #define SCI2_ERI_IRQn          ((IRQn_Type) 3) /* SCI2 ERI (Receive error) */
+        /* The number of entries required for the ICU vector table. */
+        #define BSP_ICU_VECTOR_NUM_ENTRIES (4)
+
+        #ifdef __cplusplus
+        }
+        #endif
+        #endif /* VECTOR_DATA_H */
